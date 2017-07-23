b1ffa5df806fdeb388b6daae83f5aae9517e9fdb
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-07-22 16:01:30 -1000

    up to 11 done

--- todo_app.sql
+++ todo_app.sql
@@ -16,3 +16,11 @@ CREATE TABLE tasks (
   updated_at timestamp without time zone DEFAULT NULL,
   completed BOOLEAN NOT NULL DEFAULT false
 );
+
+ALTER TABLE tasks DROP COLUMN completed;
+
+ALTER TABLE tasks ADD COLUMN completed_at timestamp without time zone NULL DEFAULT NULL;
+
+ALTER TABLE tasks ALTER updated_at SET NOT NULL;
+
+ALTER TABLE tasks ALTER updated_at SET DEFAULT NOW();
